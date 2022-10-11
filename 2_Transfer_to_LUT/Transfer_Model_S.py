9b07839b346704e18547345733bfd35020a95250
varun-jois <varunrameshjois@gmail.com>
2022-10-11 16:11:40 -0400

    Changed the saved model file path for transfering into the LUT.

--- 2_Transfer_to_LUT/Transfer_Model_S.py
+++ 2_Transfer_to_LUT/Transfer_Model_S.py
@@ -8,7 +8,7 @@ import numpy as np
 
 # USER PARAMS
 UPSCALE = 4                  # upscaling factor
-MODEL_PATH = "./Model_S.pth"    # Trained SR net params
+MODEL_PATH = '/home/varun/fvc/SR-LUT/1_Train_deep_model/model_G_i199000.pth'    # Trained SR net params  "./Model_S.pth"
 SAMPLING_INTERVAL = 4        # N bit uniform sampling
 
 
