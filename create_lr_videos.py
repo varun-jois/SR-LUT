b3d9ac4f137c7505663cdc9a42ed438079169bd4
varun-jois <varunrameshjois@gmail.com>
2022-10-23 19:55:50 -0400

    Created a script to create lr videos using ffmpeg. (2) Added an enumerate.

--- create_lr_videos.py
+++ create_lr_videos.py
@@ -5,7 +5,7 @@ import glob
 root_hr = '/home/varun/fvc/datasets/vox2_test_mp4'
 root_lr = '/home/varun/fvc/datasets/vox2_test_mp4_lr_x4'
 
-for iu in glob.iglob(f'{root_hr}/*/*/*'):
+for n, iu in enumerate(glob.iglob(f'{root_hr}/*/*/*')):
     _, id, vid, utt = iu.rsplit('/', 3)
     vpath = os.path.join(root_lr, id, vid)
     # check if the dirs have been created already
@@ -14,3 +14,4 @@ for iu in glob.iglob(f'{root_hr}/*/*/*'):
     ou = os.path.join(vpath, utt)
     # create the video using ffmpeg
     os.system(f'ffmpeg -i {iu} -vf scale=w=iw/4:h=ih/4 -c:v libx264 -preset veryfast {ou}')
+    print(f'{n} done')
