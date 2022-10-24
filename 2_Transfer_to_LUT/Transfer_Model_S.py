7c2c1e60b2024d6ba9076a854aeae8acb80611a5
varun-jois <varunrameshjois@gmail.com>
2022-10-24 12:23:44 -0400

    Changed the LUT name for the new model.

--- 2_Transfer_to_LUT/Transfer_Model_S.py
+++ 2_Transfer_to_LUT/Transfer_Model_S.py
@@ -8,9 +8,9 @@ import numpy as np
 
 # USER PARAMS
 UPSCALE = 4                  # upscaling factor
-MODEL_PATH = '/home/varun/fvc/SR-LUT/1_Train_deep_model/checkpoint/S/model_G_i199000.pth'    # Trained SR net params  "./Model_S.pth"
+MODEL_PATH = '/home/varun/fvc/SR-LUT/1_Train_deep_model/checkpoint/S/model_G_i200000.pth'    # Trained SR net params  "./Model_S.pth"
 SAMPLING_INTERVAL = 4        # N bit uniform sampling
-LUT_NAME = 'Model_S_faces'
+LUT_NAME = 'Model_S_faces_h264'
 
 
 
