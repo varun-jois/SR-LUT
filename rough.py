5a7e827d76282756f4481262043406955bf806a1
varun-jois <varunrameshjois@gmail.com>
2022-11-17 20:59:29 -0500

    Updated rough to create random data for testing the model.

--- rough.py
+++ rough.py
@@ -5,6 +5,7 @@ import numpy as np
 import cv2 as cv
 from glob import glob
 from PIL import Image
+import os
 #
 # import sys
 # sys.path.insert(1, '1_Train_deep_model')
@@ -70,51 +71,72 @@ from PIL import Image
 # test_ids = ['id08911', 'id04119', 'id07354']
 #
 
-import sys
-sys.path.insert(1, '3_Test_using_LUT')
-from lut_sr import sr
+# import sys
+# sys.path.insert(1, '3_Test_using_LUT')
+# from lut_sr import sr
+#
+# IDS_PATH = '/home/varun/PhD/Face Video Compression/SR-LUT/3_Test_using_LUT/vids/exp_2'
+# LUT_PATH = '/home/varun/PhD/Face Video Compression/SR-LUT/2_Transfer_to_LUT/LUTs/Model_S_faces_h264.npy'
+# # IDS_PATH = '/home/varun/PhD/datasets/VoxCeleb2/vox2_test_mp4/mp4'  # '/home/varun/fvc/datasets/vox2_test_mp4'
+# # LUT_PATH = '/home/varun/PhD/Face Video Compression/SR-LUT/3_Test_using_LUT/Model_S_faces.npy'  #  '/home/varun/PhD/Face Video Compression/SR-LUT/3_Test_using_LUT/Model_S_faces.npy'
+# UTT_NAMES = ['id04119_1uH67UruKlE_00002', 'id07354_iUUpvrP-gzQ_00348', 'id08911_8QeBl-d07ik_00039']
+# OUT_PATH = IDS_PATH
+#
+# for i, utt_name in enumerate(UTT_NAMES, 1):
+#     upath = join(IDS_PATH, f'{utt_name}_lut.mp4')
+#
+#     # video file to read
+#     cap = cv.VideoCapture(upath)
+#     f = 1
+#
+#     # video files for the output
+#     fourcc = cv.VideoWriter_fourcc(*'mp4v')  # mp4v
+#     # movie_bic = cv.VideoWriter(join(OUT_PATH, f'{id}_{vid}_{utt}_bic.mp4'), fourcc, 25, (224, 224))
+#     movie_lut = cv.VideoWriter(join(OUT_PATH, f'{utt_name}_lut16.mp4'), fourcc, 25, (896, 896))
+#     while True:
+#         # Capture frame-by-frame
+#         ret, lr = cap.read()
+#         if ret:
+#             # get the LR image
+#             # lr = cv.resize(frame, (0, 0), fx=1 / 4, fy=1 / 4, interpolation=cv.INTER_CUBIC)
+#             # movie_lr.write(lr)
+#             # get bicubic upsampling
+#             # sr_bic = cv.resize(lr, (0, 0), fx=4, fy=4, interpolation=cv.INTER_CUBIC)
+#             # # sr_bic = cv.resize(sr_bic, (0, 0), fx=4, fy=4, interpolation=cv.INTER_CUBIC)
+#             # movie_bic.write(sr_bic)
+#             # get lut output
+#             sr_lut = sr(lr[..., ::-1], LUT_PATH)
+#             # sr_lut = sr(sr_lut[..., ::-1], LUT_PATH)
+#             movie_lut.write(sr_lut)
+#             print(f'{f} frames done')
+#             f += 1
+#         else:
+#             break
+#     # When everything done, release the capture
+#     cap.release()
+#     # movie_bic.release()
+#     movie_lut.release()
+#     cv.destroyAllWindows()
+#
+#     print(f'{i} of {len(UTT_NAMES)} done')
 
-IDS_PATH = '/home/varun/PhD/Face Video Compression/SR-LUT/3_Test_using_LUT/vids/exp_2'
-LUT_PATH = '/home/varun/PhD/Face Video Compression/SR-LUT/2_Transfer_to_LUT/LUTs/Model_S_faces_h264.npy'
-# IDS_PATH = '/home/varun/PhD/datasets/VoxCeleb2/vox2_test_mp4/mp4'  # '/home/varun/fvc/datasets/vox2_test_mp4'
-# LUT_PATH = '/home/varun/PhD/Face Video Compression/SR-LUT/3_Test_using_LUT/Model_S_faces.npy'  #  '/home/varun/PhD/Face Video Compression/SR-LUT/3_Test_using_LUT/Model_S_faces.npy'
-UTT_NAMES = ['id04119_1uH67UruKlE_00002', 'id07354_iUUpvrP-gzQ_00348', 'id08911_8QeBl-d07ik_00039']
-OUT_PATH = IDS_PATH
 
-for i, utt_name in enumerate(UTT_NAMES, 1):
-    upath = join(IDS_PATH, f'{utt_name}_lut.mp4')
+lrpath = '1_Train_deep_model/overfit/colors/DIV2K_train_LR_bicubic/X4'
+hrpath = '1_Train_deep_model/overfit/colors/DIV2K_train_HR'
+if not os.path.exists(lrpath):
+    os.makedirs(lrpath)
+if not os.path.exists(hrpath):
+    os.makedirs(hrpath)
 
-    # video file to read
-    cap = cv.VideoCapture(upath)
-    f = 1
+a = np.zeros((48, 48, 3), dtype=np.uint8)
+a[..., 0] = 255
+a[..., 1] = 255
+Image.fromarray(a[:24, :24, :], 'RGB').save('timg.png')
 
-    # video files for the output
-    fourcc = cv.VideoWriter_fourcc(*'mp4v')  # mp4v
-    # movie_bic = cv.VideoWriter(join(OUT_PATH, f'{id}_{vid}_{utt}_bic.mp4'), fourcc, 25, (224, 224))
-    movie_lut = cv.VideoWriter(join(OUT_PATH, f'{utt_name}_lut16.mp4'), fourcc, 25, (896, 896))
-    while True:
-        # Capture frame-by-frame
-        ret, lr = cap.read()
-        if ret:
-            # get the LR image
-            # lr = cv.resize(frame, (0, 0), fx=1 / 4, fy=1 / 4, interpolation=cv.INTER_CUBIC)
-            # movie_lr.write(lr)
-            # get bicubic upsampling
-            # sr_bic = cv.resize(lr, (0, 0), fx=4, fy=4, interpolation=cv.INTER_CUBIC)
-            # # sr_bic = cv.resize(sr_bic, (0, 0), fx=4, fy=4, interpolation=cv.INTER_CUBIC)
-            # movie_bic.write(sr_bic)
-            # get lut output
-            sr_lut = sr(lr[..., ::-1], LUT_PATH)
-            # sr_lut = sr(sr_lut[..., ::-1], LUT_PATH)
-            movie_lut.write(sr_lut)
-            print(f'{f} frames done')
-            f += 1
-        else:
-            break
-    # When everything done, release the capture
-    cap.release()
-    # movie_bic.release()
-    movie_lut.release()
-    cv.destroyAllWindows()
-
-    print(f'{i} of {len(UTT_NAMES)} done')
+for i in range(120):
+    a = np.zeros((224, 224, 3), dtype=np.uint8)
+    a[..., 0] = np.random.randint(256)
+    a[..., 1] = np.random.randint(256)
+    a[..., 2] = np.random.randint(256)
+    Image.fromarray(a, 'RGB').save(f'{hrpath}/{i}_GT.png')
+    Image.fromarray(a[:56, :56, :], 'RGB').save(f'{lrpath}/{i}.png')
