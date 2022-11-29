36626f19cd28e2082a824d5d59bd08a9aa68f581
varun-jois <varunrameshjois@gmail.com>
2022-11-29 10:43:11 -0500

    Testing the output of perceptual loss.

--- 3_Test_using_LUT/build_video_from_frames.py
+++ 3_Test_using_LUT/build_video_from_frames.py
@@ -2,14 +2,14 @@ from os.path import join
 import cv2 as cv
 from lut_sr import sr
 
-id = 'x4_4bit_int8'  # colors, id04119, x4_4bit_int8
+id = 'S'  # S, colors, id04119, x4_4bit_int8
 
 IDS_PATH = '/home/varun/fvc/datasets/vox2_test_mp4_lr_x4'
 LUT_PATH = f'/home/varun/fvc/SR-LUT/2_Transfer_to_LUT/LUTs/Model_S_{id}.npy'
 # IDS_PATH = '/home/varun/PhD/datasets/VoxCeleb2/vox2_test_mp4/mp4'  # '/home/varun/fvc/datasets/vox2_test_mp4'
 # LUT_PATH = '/home/varun/PhD/Face Video Compression/SR-LUT/3_Test_using_LUT/Model_S_faces.npy'  #  '/home/varun/PhD/Face Video Compression/SR-LUT/3_Test_using_LUT/Model_S_faces.npy'
-# UTT_NAMES = ['id04119_1uH67UruKlE_00002_10', 'id07354_iUUpvrP-gzQ_00348_10', 'id08911_8QeBl-d07ik_00039_10']
-UTT_NAMES = ['id04119_1uH67UruKlE_00002_10']
+UTT_NAMES = ['id04119_1uH67UruKlE_00002_10', 'id07354_iUUpvrP-gzQ_00348_10', 'id08911_8QeBl-d07ik_00039_10']
+# UTT_NAMES = ['id04119_1uH67UruKlE_00002_10']
 OUT_PATH = 'vids'
 
 for i, utt_name in enumerate(UTT_NAMES, 1):
